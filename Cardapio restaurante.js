a22754ea3b7a3be320e65f0918a08dfea2cefa64
NgrCristian <128868075+NgrCristian@users.noreply.github.com>
2023-04-24 23:30:33 -0300

    Update Cardapio restaurante.js

--- Cardapio restaurante.js
+++ Cardapio restaurante.js
@@ -15,12 +15,15 @@ Total a Pagar:${(quantidade*valor[0]).toFixed(2)}'); break;
 Total a Pagar:${(quantidade*valor[1]).toFixed(2)}"); break; 
 
   case '3':
-console.log("Produto Selecionado:${cardapio}\r\nNome do Produto:${lanche[2]}\r\nPreço Unitario:${(valor[2]).toFixed(2)}
-17 Total a Pagar:${(quantidade*valor[2]).toFixed(2)}`); break; 
+    console.log("Produto Selecionado:${cardapio}\r\nNome do Produto:${lanche[2]}\r\nPreço Unitario:${(valor[2]).toFixed(2)}
+Total a Pagar:${(quantidade*valor[2]).toFixed(2)}`); break; 
   
   case '4':
-console.log("Produto Selecionado:${cardapio}\r\nNome do Produto:${lanche[3]}\r\nPreço Unitario:${(valor[3]).toFixed(2)} Total a Pagar:${(quantidade*valor [3]).toFixed(2)}"); break; 
+    console.log("Produto Selecionado:${cardapio}\r\nNome do Produto:${lanche[3]}\r\nPreço Unitario:${(valor[3]).toFixed(2)} 
+Total a Pagar:${(quantidade*valor [3]).toFixed(2)}"); break; 
 
   case '5':
-console.log("Produto Selecionado:${cardapio}\r\nNome do Produto:${lanche[4]}\r\nPreço Unitario:${(valor[4]).toFixed(2)} Total a Pagar:${(quantidade*valor [4]).toFixed(2)}');
+    console.log("Produto Selecionado:${cardapio}\r\nNome do Produto:${lanche[4]}\r\nPreço Unitario:${(valor[4]).toFixed(2)} 
+Total a Pagar:${(quantidade*valor [4]).toFixed(2)}');
+
 }
